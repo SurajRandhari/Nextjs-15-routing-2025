0df9f76c1f513e712ba75c528b7035ba625a6ebf
Suraj Randhari <surajrandhari@gmail.com>
2025-04-14 23:54:35 +0530

    folder routing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,7 @@
-
-export default function Home() {
+export default function About() {
   return (
     <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h2>This is Home page</h2>
+      <h2>This is Aboout page</h2>
     </div>
   );
 }
