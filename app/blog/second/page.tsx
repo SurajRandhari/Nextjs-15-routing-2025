f24d8278e3f2a67848219c1250afa63a9b91897c
Suraj Randhari <surajrandhari@gmail.com>
2025-04-15 23:48:40 +0530

    Nested Route

---
+++ app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+      <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h2>This is blog-Second page</h2>
+      </div>
+    );
+  }
